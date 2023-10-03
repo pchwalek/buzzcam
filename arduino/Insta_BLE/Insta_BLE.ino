dcfd231539b4a04471a57f921fe6ca978d314710
Patrick Chwalek <patrickchwalek@gmail.com>
2023-10-03 14:41:45 -0400

    added protobuf files and template Arduino code

---
+++ arduino/Insta_BLE/Insta_BLE.ino
@@ -0,0 +1,260 @@
+/*
+    Author: Patrick Chwalek (09/22/2023)
+    Description: Example code used to control Insta360 X3 and RS 1-inch cameras
+*/
+
+#include <BLE2902.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLEAdvertising.h>
+
+#include "pb.h"
+#include "message.pb.h"
+#include "pb_encode.h"
+
+#define SERVICE_UUID "ce70"
+#define CHARACTERISTIC_UUID1 "ce71"
+#define CHARACTERISTIC_UUID2 "ce72"
+#define CHARACTERISTIC_UUID3 "ce73"
+
+Packet message;
+uint8_t buffer[400];
+
+uint32_t data_32;
+uint16_t data_16;
+uint8_t data_8[30];
+
+BLEServer *pServer = NULL;
+BLE2902 *pDescriptor2902;
+
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+
+BLECharacteristic *pCharacteristicRx;
+
+unsigned long myTime;
+
+class MyServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer *pServer) {
+        deviceConnected = true;
+        myTime = millis();
+        BLEDevice::startAdvertising();
+    };
+
+    void onDisconnect(BLEServer *pServer) { deviceConnected = false; }
+};
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println("Starting BLE work!");
+
+    BLEDevice::init("BuzzCam");
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
+
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+    // BLECharacteristic *pCharacteristic1 = pService->createCharacteristic(
+    //     CHARACTERISTIC_UUID1, BLECharacteristic::PROPERTY_WRITE || BLECharacteristic::PROPERTY_READ);
+
+    pCharacteristicRx = pService->createCharacteristic(
+        CHARACTERISTIC_UUID2,  BLECharacteristic::PROPERTY_NOTIFY);
+    pDescriptor2902 = new BLE2902();
+    pDescriptor2902->setNotifications(true);
+    pDescriptor2902->setIndications(true);
+    pCharacteristicRx->addDescriptor(pDescriptor2902);
+    data_8[0] = 0;
+    pCharacteristicRx->setValue(data_8, 1);
+
+    // system state read value
+    BLECharacteristic *pCharacteristic3 = pService->createCharacteristic(
+        CHARACTERISTIC_UUID3, BLECharacteristic::PROPERTY_WRITE);
+    data_16 = 0x0201;
+
+  
+
+    pService->start();
+    // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still
+    // is working for backward compatibility
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    // pAdvertising->addServiceUUID(SECONDARY_SERVICE_UUID);
+
+    pAdvertising->setScanResponse(true);
+    pAdvertising->setMinPreferred(
+        0x06); // functions that help with iPhone connections issue
+    pAdvertising->setMinPreferred(0x12);
+
+    BLEDevice::startAdvertising();
+    Serial.println(
+        "Characteristic defined! Now you can read it in your phone!");
+
+
+
+
+    /* PROTOBUF SPECIFIC */
+    message.has_header = true;
+    message.header.epoch = 1213232; // unix timestamp
+    message.header.ms_from_start = millis();
+    message.header.system_uid = 0x1234; // UID
+    message.which_payload = Packet_system_info_packet_tag; // packet type is system_info
+    message.payload.system_info_packet.device_recording = false;
+    message.payload.system_info_packet.number_discovered_devices = 0;
+    message.payload.system_info_packet.has_mark_state = true;
+    message.payload.system_info_packet.mark_state.mark_number = 0;
+    message.payload.system_info_packet.mark_state.beep_enabled = false;
+    message.payload.system_info_packet.mark_state.timestamp_unix = 1232422;
+    message.payload.system_info_packet.has_sdcard_state = true;
+    message.payload.system_info_packet.sdcard_state.detected = 1;
+    message.payload.system_info_packet.sdcard_state.space_remaining = 123412342;
+    message.payload.system_info_packet.sdcard_state.estimated_remaining_recording_time = 214332;
+    message.payload.system_info_packet.has_simple_sensor_reading = true;
+    message.payload.system_info_packet.simple_sensor_reading.timestamp_unix = 12312421;
+    message.payload.system_info_packet.simple_sensor_reading.co2 = 300;
+    message.payload.system_info_packet.simple_sensor_reading.humidity = 40;
+    message.payload.system_info_packet.simple_sensor_reading.temperature = 70;
+    message.payload.system_info_packet.simple_sensor_reading.index = 3;
+
+    // define stream and encode
+    pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+    pb_encode(&stream, Packet_fields, &message);
+
+    pCharacteristic3->setValue(buffer, stream.bytes_written);
+}
+
+void loop() {
+    // notify changed value
+    if (deviceConnected) {
+        delay(10); // bluetooth stack will go into congestion, if too many packets
+    }
+
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("start advertising");
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+        // do stuff here on connecting
+        Serial.println("connecting");
+        oldDeviceConnected = deviceConnected;
+    }
+
+    /* this loop just goes through all the features */
+    // if (((millis() - myTime) > CAPTURE_DELAY)) {
+    //     Serial.println("capture");
+    //     myTime = millis();
+        // shutterButton(pCharacteristicRx);
+        // delay(30000);
+
+        // Serial.println("screen off");
+        // screenToggle(pCharacteristicRx);
+        // delay(5000);
+
+        // Serial.println("screen on");
+        // screenToggle(pCharacteristicRx);
+        // delay(5000);
+        
+        // Serial.println("power off");
+        // powerOff(pCharacteristicRx);
+        // delay(30000);
+
+    //     Serial.println("power on");
+    //     BLEDevice::stopAdvertising();
+    //     powerOnPrevConnectedCameras();
+    // }
+}
+
+
+
+
+
+// void screenToggle(BLECharacteristic *characteristic) {
+//     data_8[0] = 0xfc;
+//     data_8[1] = 0xef;
+//     data_8[2] = 0xfe;
+//     data_8[3] = 0x86;
+//     data_8[4] = 0x00;
+//     data_8[5] = 0x03;
+//     data_8[6] = 0x01;
+//     data_8[7] = 0x00;
+//     data_8[8] = 0x00;
+//     characteristic->setValue(data_8, 9);
+//     characteristic->notify();
+// }
+
+// void powerOff(BLECharacteristic *characteristic) {
+//     data_8[0] = 0xfc;
+//     data_8[1] = 0xef;
+//     data_8[2] = 0xfe;
+//     data_8[3] = 0x86;
+//     data_8[4] = 0x00;
+//     data_8[5] = 0x03;
+//     data_8[6] = 0x01;
+//     data_8[7] = 0x00;
+//     data_8[8] = 0x03;
+//     characteristic->setValue(data_8, 9);
+//     characteristic->notify();
+// }
+
+// void powerOnPrevConnectedCameras() {
+//     /* the below might be camera specific and you may need to sniff them yourself for your camera */
+
+//     /* used for Insta360 X3 */
+//     // manuf_data[14] = 0x37;
+//     // manuf_data[15] = 0x4b;
+//     // manuf_data[16] = 0x43;
+//     // manuf_data[17] = 0x4d;
+//     // manuf_data[18] = 0x54;
+//     // manuf_data[19] = 0x4b;
+
+//     /* used for Insta360 RS 1-inch */
+//     manuf_data[14] = 0x38;
+//     manuf_data[15] = 0x51;
+//     manuf_data[16] = 0x53;
+//     manuf_data[17] = 0x4a;
+//     manuf_data[18] = 0x38;
+//     manuf_data[19] = 0x52;
+
+//     // BLEAdvertisementData advertisementData;
+//     // advertisementData.setName("Insta360 GPS Remote");
+//     // advertisementData.setFlags(ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_DMT_CONTROLLER_SPT | ESP_BLE_ADV_FLAG_DMT_HOST_SPT);
+//     // advertisementData.setManufacturerData((char *) manuf_data);
+
+//     // BLEAdvertising* advertisement = new BLEAdvertising();
+//     // advertisement->setAdvertisementData(advertisementData);
+// 	  // advertisement->start();
+
+//     // custom function that adds manufacturing data and modifies flags of advertisement
+//     BLEDevice::startAdvertisingWithManufData(manuf_data);
+// }
+
+// void modeButton(BLECharacteristic *characteristic) {
+//     data_8[0] = 0xfc;
+//     data_8[1] = 0xef;
+//     data_8[2] = 0xfe;
+//     data_8[3] = 0x86;
+//     data_8[4] = 0x00;
+//     data_8[5] = 0x03;
+//     data_8[6] = 0x01;
+//     data_8[7] = 0x01;
+//     data_8[8] = 0x00;
+//     characteristic->setValue(data_8, 9);
+//     characteristic->notify();
+// }
+
+// void shutterButton(BLECharacteristic *characteristic) {
+//     data_8[0] = 0xfc;
+//     data_8[1] = 0xef;
+//     data_8[2] = 0xfe;
+//     data_8[3] = 0x86;
+//     data_8[4] = 0x00;
+//     data_8[5] = 0x03;
+//     data_8[6] = 0x01;
+//     data_8[7] = 0x02;
+//     data_8[8] = 0x00;
+//     characteristic->setValue(data_8, 9);
+//     characteristic->notify();
+// }
