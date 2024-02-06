5f52c8d03cc666f75e0f23b4864bcf0a20487c85
IsamarZhu <isamar88888@gmail.com>
2024-02-06 18:37:31 -0500

    Added basic splash screen

--- Swift Code/BuzzCam App/ContentView.swift
+++ Swift Code/BuzzCam App/ContentView.swift
@@ -12,22 +12,58 @@ struct ContentView: View {
     @StateObject var bluetoothModel = BluetoothModel() // Initialize bluetoothModel, will be passed down to children
     @State var connected = false // When connected, display correct view, default show DiscoverView
     
+    @State private var showMainContent = false // Bool for splash screen
+    
     var body: some View {
-        if connected { // if connected, show main view
-            TabView {
-                MainView(connected: $connected).environmentObject(bluetoothModel)
-                    .tabItem {
-                        Label("Main", systemImage: "house.fill")
+        Group {
+            if showMainContent {
+                if connected { // if connected, show main view
+                    TabView {
+                        MainView(connected: $connected).environmentObject(bluetoothModel)
+                            .tabItem {
+                                Label("Main", systemImage: "house.fill")
+                            }
+                        
+                        ConfigView().environmentObject(bluetoothModel)
+                            .tabItem {
+                                Label("Config", systemImage: "gearshape.fill")
+                            }
                     }
-                
-                ConfigView().environmentObject(bluetoothModel)
-                    .tabItem {
-                        Label("Config", systemImage: "gearshape.fill")
-                    }
+                }
+                else { // show discover devices
+                    DiscoverView(connected: $connected).environmentObject(bluetoothModel)
+                }
             }
-        }
-        else { // show discover devices
-            DiscoverView(connected: $connected).environmentObject(bluetoothModel)
+            else { // Show splash screen
+                // Launch screen
+                ZStack {
+                    Color(red: 40/255, green: 35/255, blue: 41/255)
+                        .edgesIgnoringSafeArea(.all) // Set the background color
+                    
+                    Image("BuzzCam Logo") // Replace "launch_image" with your image name
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .edgesIgnoringSafeArea(.all)
+                        .aspectRatio(contentMode: .fill)
+                    
+                    VStack(spacing: 20) {
+                        Spacer().frame(height: 400)
+                        Text("BuzzCam")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                    }
+                }
+                .onAppear {
+                    // Show launch screen for 3 seconds
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                        withAnimation {
+                            self.showMainContent = true
+                        }
+                    }
+                }
+            }
         }
         
     }
